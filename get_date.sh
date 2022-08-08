e2dead3a2f04bd067150682fcbc6a52e56def73c
Megha Mishra <102301838+Realmeghamishra@users.noreply.github.com>
2022-08-08 14:50:46 +0530

    Create get_date.sh

---
+++ get_date.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NOW=$(date +"%m-%d-%Y")
+FILE="backup.$NOW.tar.gz"
+echo "Backing up data to /nas42/backup.$NOW.tar.gz file, please wait..."
+# rest of script
+# tar xcvf /nas42/backup.$NOW.tar.gz /home/ /etc/ /var
